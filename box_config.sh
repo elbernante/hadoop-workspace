85347769f471c264aaca84305ef9321e94cea301
Peter James Bernante <elbernante@gmail.com>
2017-04-27 17:42:30 -0500

    Move installation location from /vagrant/local to /vagrant/_local

--- box_config.sh
+++ box_config.sh
@@ -11,7 +11,7 @@ vagrantTip="[35m[1mThe shared directory is located at /vagrant\nTo access your
 echo -e $vagrantTip > /etc/motd
 
 # Create directory for installation destinations
-VAGRANT_LOCAL=/vagrant/local
+VAGRANT_LOCAL=/vagrant/_local
 mkdir -p ${VAGRANT_LOCAL}
 
 # Empty installation destinations in case it was already created
